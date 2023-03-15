a07b71bea291151caed405c54430503c91cff7b6
ssolunna <ssolunna29@gmail.com>
2023-03-15 14:58:42 -0500

    Allow player to make guesses until out of chances

--- hangman.rb
+++ hangman.rb
@@ -10,30 +10,51 @@ def select_random_word(dictionary)
   dictionary.filter { |word| word.length > 4 && word.length < 13 }.sample
 end
 
-def display_word(word, guess)
+def display_word(word, correct_letters)
   print ' Word: '
-  puts word.gsub(/[^#{guess}]/, '_') # Display only the correct letter guess
+
+  if correct_letters.empty?
+    word.length.times { print '_' }
+  else
+    # Displays only the correct letter guess
+    print word.gsub(/[^#{correct_letters.join}]/, '_')
+  end
+
+  puts
 end
 
-def display_incorrect_letters(word, guess)
-  wrong_guesses = []
-  wrong_guesses << guess unless word.include?(guess)
-  puts "Wrong guesses: '#{wrong_guesses.join}'"
+def make_guess
+  print 'Guess: '
+  gets.chomp.downcase
+end
+
+def play(word, chances, incorrect_letters, correct_letters)
+  until chances.zero?
+    guess = make_guess
+
+    if word.include?(guess)
+      correct_letters << guess unless correct_letters.include?(guess)
+    else
+      chances -= 1 # Counts the remaining incorrect guesses
+      incorrect_letters << guess unless incorrect_letters.include?(guess)
+    end
+
+    display_word(word, correct_letters)
+
+    puts "Wrong guesses: '#{incorrect_letters.join(' ')}'"
+
+    puts "Chances left: #{chances}"
+  end
 end
 
 dictionary = load_dictionary('dictionary.txt')
 
 word = select_random_word(dictionary)
 
-chances_left = 5 # Represents the num of incorrect guesses allowed
+chances = 5 # Represents the num of incorrect guesses allowed
 
-guess = 'a'
+correct_letters = []
 
-display_word(word, guess)
+incorrect_letters = []
 
-# Counts the remaining incorrect guesses
-chances_left -= 1 unless word.include?(guess)
-
-puts "Chances left: #{chances_left}"
-
-display_incorrect_letters(word, guess)
+play(word, chances, incorrect_letters, correct_letters)
